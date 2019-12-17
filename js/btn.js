5b0614cdf71887161ad240c799068d18522f3191
rhanlin <spencerlin999@gmail.com>
2019-12-17 22:15:49 +0800

    環境建立！！

---
+++ js/btn.js
@@ -0,0 +1,13 @@
+let btn = document.querySelectorAll('.btn');
+// console.log(btn[0]);
+btn[0].addEventListener('mouseup',btnFn);
+btn[1].addEventListener('mouseup',btnFn);
+btn[2].addEventListener('mouseup',btnFn);
+
+function btnFn(e){
+    // console.log(e.target);
+    e.target.classList.add("clicked");
+    setTimeout(()=>{
+        e.target.classList.remove("clicked");
+    },300)
+}
