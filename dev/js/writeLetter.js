4874eb63f9331ba7d49c5f719feb9a7c0c1d1d11
rhanlin <spencerlin999@gmail.com>
2020-01-02 22:05:17 +0800

    write-paper 頁面完成(剩下RWD)

--- dev/js/writeLetter.js
+++ dev/js/writeLetter.js
@@ -1,16 +1,7 @@
-let previewPattern = document.getElementById('previewPattern');
-let painteTypeBtn = document.querySelectorAll("#painteType > .type");
+// let previewPattern = document.getElementById('previewPattern');
+// let painteTypeBtn = document.querySelectorAll("#painteType > .type");
+// let letterImg = document.getElementById('letterImg');
 
-console.log(painteTypeBtn);
-
-for(let i = 0 ; i<painteTypeBtn.length ; i++){
-  painteTypeBtn[i].addEventListener('click',(e)=>{
-    //解掉冒泡事件
-    e.stopPropagation();
-    // e.stopImmediatePropagation();
-    // previewPattern.style.backgroundImage = `url(./img/pattern/plan-pattern-1.svg)`;
-    // console.log(e.target.style);
-    // console.log(e.currentTarget.querySelectorAll(".print-type-pattern"));//.....??
-  })
-}
-
+// //上傳圖片換圖
+// letterImg.addEventListener('click', (e)=>{
+// } ,false);
