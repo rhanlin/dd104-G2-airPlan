b2f0a7ef1dcefbf914581bd4b99708058a1e6763
musharon <lovely30539@gmail.com>
2020-01-08 21:32:28 +0800

    聊天室 PHP可撈到聊天室列表

--- dev/js/chatroom.php
+++ dev/js/chatroom.php
@@ -5,27 +5,32 @@ try {
     $password = "qwert2321";
     $options = array(PDO::ATTR_CASE => PDO::CASE_NATURAL, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_ORACLE_NULLS => PDO::NULL_NATURAL);
     $pdo = new PDO($dsn, $user, $password, $options);
-    $type = $_POST["type"];
+    $type = "mark";
     switch ($type) {
-        case "chat":
-            $test = array("type" => "type", "memNo1" => "memNo1");
-            $memNo1 = $_POST["memNo1"];
-            $memNo2 = $_POST["memNo2"];
-            $chatNo = $_POST["chatNo"];
-            $sql = "select * from chat where ((memNo1 = :memNo1 and memNo2 = :memNo2) or (memNo2 = :memNo1 and memNo1 = :memNo2)) and chatNo > :chatNo";
-            $content = $pdo->prepare($sql);
-            $content->bindValue(":memNo1", $memNo1);
-            $content->bindValue(":memNo2", $memNo2);
-            $content->bindValue(":chatNo", $chatNo);
-            $content->execute();
-            $chat = $content->fetchAll(PDO::FETCH_ASSOC);
-            echo json_encode($chat);
-            break;
+        // case "chat":
+        //     $test = array("type" => "type", "memNo1" => "memNo1");
+        //     $memNo1 = $_POST["memNo1"];
+        //     $memNo2 = $_POST["memNo2"];
+        //     $chatNo = $_POST["chatNo"];
+        //     $sql = "select * from chat where ((memNo1 = :memNo1 and memNo2 = :memNo2) or (memNo2 = :memNo1 and memNo1 = :memNo2)) and chatNo > :chatNo";
+        //     $content = $pdo->prepare($sql);
+        //     $content->bindValue(":memNo1", $memNo1);
+        //     $content->bindValue(":memNo2", $memNo2);
+        //     $content->bindValue(":chatNo", $chatNo);
+        //     $content->execute();
+        //     $chat = $content->fetchAll(PDO::FETCH_ASSOC);
+        //     echo json_encode($chat);
+        //     break;
 
 
         case "mark":
-            $memNo1 = $_POST["memNo1"];
-            $sql = "select * from chat where memNo1 = :memNo1 ";
+            // $memNo1 = $_POST["memNo1"];
+            $memNo1 = 1;
+            // $mname = $_POST["ename"];
+            $sql = "select a.memNo,b.memNo,b.memName,matPosUrl,mugStatus
+                    from `chat` join`member` a on(chat.memNo1=a.memNo)join `member`b on(chat.memNo2 = b.memNo) join `matpostmark` on (a.memNo = matpostmark.memNo)
+                    where a.memNo = 1 and mugStatus = 1
+                    order by a.memNo;";
             $content = $pdo->prepare($sql);
             $content->bindValue(":memNo1", $memNo1);
             $content->execute();
