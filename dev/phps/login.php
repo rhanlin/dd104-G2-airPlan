e2ab094bcfe54b12943fb82ef582224ef7667f3e
kang056 <a0926036356@gmail.com>
2020-01-11 12:39:47 +0800

    登入功能調整完成

--- dev/phps/login.php
+++ dev/phps/login.php
@@ -1,28 +1,25 @@
-<!-- 登入 -->
 <?php 
 try {
-    require_once "connectBook_userSetting.php";
-    $sql = "select * from `member` where memEmail='{$_POST["memEmail"]}'";
-    $sql2 = "select * from `member` where memEmail='{$_POST["memEmail"]}' and memPsw='{$_POST["memPsw"]}'";
-    $memberE = $pdo->query($sql);
-    $memberP = $pdo->query($sql2);
-    $row_memberE = $memberE->fetch(PDO::FETCH_ASSOC);
-    $row_memberP = $memberP->fetch(PDO::FETCH_ASSOC);
-	if($row_memberE){
-        if($row_memberP){
-            echo "<script>alert(`登入成功`); location.href = '../home.html';</script>";
-            // echo '<meta http-equiv=REFRESH CONTENT=1;url=member.php>';
-        }
-        else{
-            echo "<script>alert(`密碼錯誤`); location.href = '../home.html';</script>";
-        }
-	}
-	else{
-        echo "<script>alert(`無此Email`); location.href = '../home.html';</script>";
-	}
-} 
-catch (PDOException $e) {
+	require_once("connectBook_userSetting.php");
+    $sql = "select * from `member` where memEmail=:memEmail and memPsw=:memPsw";
+	$member = $pdo->prepare($sql);
+	$member->bindValue(':memEmail', $_POST["memEmail"]);
+	$member->bindValue(':memPsw', $_POST["memPsw"]);
+	$member->execute();
+	if( $member->rowCount() == 0 ){ //找不到
+      echo "{}";//傳回空的JSON字串
+    }else{ //找得到
+      	$memRow = $member->fetch(PDO::FETCH_ASSOC);//取回一筆資料
+        session_start();//登入成功,將登入者的資料寫入session
+        $_SESSION["memEmail"] = $memRow["memEmail"];
+	    $_SESSION["memNo"] = $memRow["memNo"];
+        $_SESSION["letCount"] = $memRow["letCount"]; 
+        $_SESSION["airCoin"] = $memRow["airCoin"];      
+      	echo json_encode($memRow);//送出json字串
+    } 
+
+} catch (PDOException $e) {
 	echo "例外行號 : ", $e->getLine(),"<br>";
-	echo "例外原因 : ", $e->getMessage(),"<br>";	
+	echo "例外原因 : ", $e->getMessage(),"<br>";		
 }
 ?>
